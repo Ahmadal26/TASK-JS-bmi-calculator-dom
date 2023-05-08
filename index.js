4f044e8bafa3219492937dc7b4befb30fde449e2
unknown <almusallam296@hotmail.com>
2023-05-08 15:19:38 -0700

    need help in Challenge 3

--- index.js
+++ index.js
@@ -8,4 +8,62 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+  //Challange#1
+let BMI = (weight/((Math.pow(height,2))/10000)); ;
+alert(BMI);
+
+
+  //Challange#2
+
+  if (BMI < 18.5) {
+    alert("Underweight");
+} else if (BMI >= 18.5 && BMI <= 24.9) {
+  alert("Healthy Weight");
+
+} else if (BMI >= 25 && BMI <= 29.9) {
+  alert("Overweight");
+} else {
+   alert("Above Obesity");
 }
+
+
+
+
+
+
+//Challenge#3 //please I need help with this part :)
+
+
+  if(age>=19 &&age<=24)
+  
+    {
+    alert("Healthy");
+  }
+
+  else if(age>=25 &&age<=34)
+  {
+    alert("Healthy");
+  }
+  else if(age>=35 &&age<=44)
+  {
+    alert("Healthy");
+  
+  }
+
+  if(age>=55 &&age<=64)
+  {
+    alert("Healthy");
+  }
+
+  else if(age>65)
+  {
+    alert(" Healthy");
+  }
+else 
+{
+  alert("Not Healthy")
+}
+
+}
+
+
